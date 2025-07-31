b2520390c1bca8bf643f6ba0576871308debae62
sungq-lee <sungq2@gmail.com>
2025-07-31 14:21:31 +0900

    [feature] 40 숫자 아닌 문자 input 처리 test

--- game.py
+++ game.py
@@ -2,5 +2,10 @@ class Game:
     def guess(self, guessNumber):
         if guessNumber is None:
             raise TypeError()
+
         if len(guessNumber) != 3:
             raise TypeError()
+
+        for number in guessNumber:
+            if not ord('0') <= ord(number) <= ord('9'):
+                raise TypeError()
