5e9977ab041870322f3b92922cc30b1de0c5f74d
sungq-lee <sungq2@gmail.com>
2025-07-31 14:25:40 +0900

    [refactor] 44 method extract assert_illegal_value

--- game.py
+++ game.py
@@ -1,15 +1,15 @@
 class Game:
     def guess(self, guessNumber):
+        self.assert_illegal_value(guessNumber)
+
+    def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
             raise TypeError()
-
         if len(guessNumber) != 3:
             raise TypeError()
-
         for number in guessNumber:
             if not ord('0') <= ord(number) <= ord('9'):
                 raise TypeError()
-
         if self.isDuplicatedNumber(guessNumber):
             raise TypeError()
 
