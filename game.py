9e826e6fb8c792a6be79cc91d84b77a91b541c84
sungq-lee <sungq2@gmail.com>
2025-07-31 14:29:19 +0900

    [refactor] 46 new line and illegal value type error message

--- game.py
+++ game.py
@@ -4,14 +4,17 @@ class Game:
 
     def assert_illegal_value(self, guess_number):
         if guess_number is None:
-            raise TypeError()
+            raise TypeError("입력이 None 입니다.")
+
         if len(guess_number) != 3:
-            raise TypeError()
+            raise TypeError("입력은 3 자리 문자열 이어야 합니다.")
+
         for number in guess_number:
             if not ord('0') <= ord(number) <= ord('9'):
-                raise TypeError()
+                raise TypeError("모든 문자는 숫자로 구성되어야 합니다.")
+
         if self.is_duplicated_number(guess_number):
-            raise TypeError()
+            raise TypeError("중복된 숫자가 존재 합니다.")
 
     def is_duplicated_number(self, guess_number):
         return (guess_number[0] == guess_number[1] or
