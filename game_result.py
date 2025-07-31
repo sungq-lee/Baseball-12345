660a4560c64cb661aa3197196bd5487b757dfb82
sungq-lee <sungq2@gmail.com>
2025-07-31 14:47:18 +0900

    [refactor] 56 Game result 클래스 property getter 설정

--- game_result.py
+++ game_result.py
@@ -3,3 +3,15 @@ class GameResult:
         self._solved = solved
         self._strikes = strikes
         self._balls = balls
+
+    @property
+    def solved(self):
+        return self._solved
+
+    @property
+    def strikes(self):
+        return self._strikes
+
+    @property
+    def balls(self):
+        return self._balls
