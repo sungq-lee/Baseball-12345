e8856738d8b2549e2ac999b6c302630b84124066
sungq-lee <sungq2@gmail.com>
2025-07-31 14:24:45 +0900

    [refactor] 43 method extract isDuplicatedNumber

--- game.py
+++ game.py
@@ -10,7 +10,10 @@ class Game:
             if not ord('0') <= ord(number) <= ord('9'):
                 raise TypeError()
 
-        if (guessNumber[0] == guessNumber[1] or
-            guessNumber[0] == guessNumber[2] or
-             guessNumber[1] == guessNumber[2]):
+        if self.isDuplicatedNumber(guessNumber):
             raise TypeError()
+
+    def isDuplicatedNumber(self, guessNumber):
+        return (guessNumber[0] == guessNumber[1] or
+                guessNumber[0] == guessNumber[2] or
+                guessNumber[1] == guessNumber[2])
