bc83136bab6db18d76ae9848a113409a062663e5
sungq-lee <sungq2@gmail.com>
2025-07-31 15:02:35 +0900

    [refactor] 68 test refactoring

--- test_game.py
+++ test_game.py
@@ -19,28 +19,18 @@ def test_exception_when_invalid_input(game, invalid_input):
     assert_illegal_argument(game, invalid_input)
 
 
-def assert_matched_number(balls, result, strikes):
+def assert_matched_number(result, solved, strikes, balls):
     assert result is not None
-    assert result.solved is False
+    assert result.solved is solved
     assert result.strikes == strikes
     assert result.balls == balls
 
 
 def test_return_solved_result_if_matched_number(game):
     game.question = "123"
-    result: GameResult = game.guess("123")
-
-    assert result is not None
-    assert result.solved is True
-    assert result.strikes == 3
-    assert result.balls == 0
+    assert_matched_number(game.guess("123"), solved=True, strikes=3, balls=0)
 
 
 def test_return_solved_result_if_unmatched_number(game):
     game.question = "123"
-    result: GameResult = game.guess("456")
-
-    strikes = 0
-    balls = 0
-
-    assert_matched_number(balls, result, strikes)
+    assert_matched_number(game.guess("456"), solved=False, strikes=0, balls=0)
