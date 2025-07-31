c7f22dc76e03f6399ab74235fc9dd0beb87ca038
sungq-lee <sungq2@gmail.com>
2025-07-31 14:09:22 +0900

    [feature] 31 test exception when input length is unmatched

--- test_game.py
+++ test_game.py
@@ -2,10 +2,17 @@ import pytest
 
 from game import Game
 
+
 @pytest.fixture()
 def game():
     return Game()
 
+
 def test_exception_when_input_is_none(game):
     with pytest.raises(TypeError):
         game.guess(None)
+
+
+def test_exception_when_length_is_unmatched(game):
+    with pytest.raises(TypeError):
+        game.guess("12")
